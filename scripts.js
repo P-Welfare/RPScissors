40cfd5dc19dc42849403c0f48103997b2695edec
P-Welfare <114190262+P-Welfare@users.noreply.github.com>
2023-01-05 16:31:01 +0700

    update final score in script file

--- scripts.js
+++ scripts.js
@@ -8,9 +8,38 @@ const arrayChoices = ["rock", "paper", "scissors"]
 function getComputerSelection() {
     computerSelection = arrayChoices[Math.floor(Math.random() * arrayChoices.length)];
 } 
+document.getElementById('results').innerHTML = `The score is You: ${playerScore} Robotman : ${computerScore}`;
 
        
 
+
+
+    
+var rock = document.getElementById('rock');
+rock.addEventListener('click', () => { 
+    (playRound('rock', computerSelection));
+   
+    
+    
+
+})
+var paper = document.getElementById('paper');
+paper.addEventListener('click', () => { 
+    (playRound('paper', computerSelection));
+    
+    
+})
+
+var scissors = document.getElementById('scissors');
+scissors.addEventListener('click', () => { 
+    (playRound('scissors', computerSelection));
+
+    
+    
+  
+
+    
+})
 
 function playRound(playerSelection, computerSelection) {
     getComputerSelection();
@@ -33,6 +62,7 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         ++computerScore;
         document.getElementById('current').innerHTML = `Current Round: You lose! Scissors Beat Paper.`;
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
         ++playerScore;
         ++computerScore
         document.getElementById('current').innerHTML = `Current Round: Its a draw!`;
@@ -46,108 +76,37 @@ function playRound(playerSelection, computerSelection) {
         ++playerScore;
         ++computerScore;
         document.getElementById('current').innerHTML = `Current Round: Its a draw!`;
-
-        
-    }
-  
     }
     
-var rock = document.getElementById('rock');
-rock.addEventListener('click', () => { 
-    console.log(playRound('rock', computerSelection));
-    if (playerScore === 5 && playerScore > computerScore) {
-        const results = document.querySelector('#results');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'Final Result: You are the winner!';
-        container.appendChild(content);
-    } else if (computerScore === 5 && computerScore > playerScore) {
-        const results = document.querySelector('#results');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'Final Result: You are the loser!';
-        container.appendChild(content);
+
         
-    } else if (computerScore === 5 && playerScore === 5) {
-        const results = document.querySelector('#results');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = `Final Result: It's a draw!`;
-        container.appendChild(content);
-    }
+    
     document.getElementById('results').innerHTML = `The score is You: ${playerScore} Robotman : ${computerScore}`;
-
-})
-var paper = document.getElementById('paper');
-paper.addEventListener('click', () => { 
-    console.log(playRound('paper', computerSelection));
     if (playerScore === 5 && playerScore > computerScore) {
         const results = document.querySelector('#results');
         const content = document.createElement('div');
         content.classList.add('content');
-        content.textContent = 'You are the winner!';
+        content.textContent = 'Result of 5: You are the winner!';
         container.appendChild(content);
+        playerScore = 0
+        computerScore = 0
     } else if (computerScore === 5 && computerScore > playerScore) {
         const results = document.querySelector('#results');
         const content = document.createElement('div');
         content.classList.add('content');
-        content.textContent = 'You are the loser!';
+        content.textContent = 'Result of 5: You are the loser!';
         container.appendChild(content);
+        playerScore = 0;
+        computerScore = 0;
         
     } else if (computerScore === 5 && playerScore === 5) {
         const results = document.querySelector('#results');
         const content = document.createElement('div');
         content.classList.add('content');
-        content.textContent = `It's a draw!`;
+        content.textContent = `Result of 5: It's a draw!`;
         container.appendChild(content);
-    }
-    document.getElementById('results').innerHTML = `The score is You: ${playerScore} Robotman : ${computerScore}`;
-})
-
-var scissors = document.getElementById('scissors');
-scissors.addEventListener('click', () => { 
-    console.log(playRound('scissors', computerSelection));
-    if (playerScore === 5 && playerScore > computerScore) {
-        const results = document.querySelector('#results');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'You are the winner!';
-        container.appendChild(content);
-    } else if (computerScore === 5 && computerScore > playerScore) {
-        const results = document.querySelector('#results');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'You are the loser!';
-        container.appendChild(content);
-        
-    } else if (computerScore === 5 && playerScore === 5) {
-        const results = document.querySelector('#results');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = `It's a draw!`;
-        container.appendChild(content);
-    }
-    document.getElementById('current').innerHTML = `The score is You: ${playerScore} Robotman : ${computerScore}`;
-  
-
+        playerScore = 0;
+        computerScore = 0;
     
-})
-
- 
-
-
-function game() {
-        getComputerSelection();
-        getPlayerSelection();
-        console.log(playRound(playerSelection, computerSelection));
-                       }
-        
-    if (playerScore > computerScore)  {
-        console.log(`You win with a score of ${playerScore} to ${computerScore}`);
-    
-    } else if (computerScore > playerScore) {
-        console.log(`You lose with a score of Robotman  ${computerScore} to You: ${playerScore}`);
-    } else {
-        console.log(`Its a draw with a score of You : ${playerScore} to Robotman ${computerScore}`)
     }
-
+}
